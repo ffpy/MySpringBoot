3beec4dd95f8d28ce116e4b93bbd8675e13577a4
Wenlongsheng <wenlongsheng@ganguo.hk>
2020-03-24 15:20:49 +0800

    更新common模块

--- common/src/main/java/com/ganguomob/dev/myspringboot/common/util/SpringContextUtils.java
+++ common/src/main/java/com/ganguomob/dev/myspringboot/common/util/SpringContextUtils.java
@@ -7,6 +7,13 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.Consumer;
+
 /**
  * Spring上下文工具类
  *
@@ -16,40 +23,122 @@ import org.springframework.stereotype.Component;
 public class SpringContextUtils implements InitializingBean, ApplicationContextAware {
 
     /** Spring环境 */
-    private static Environment ENVIRONMENT;
+    private volatile static Environment environment;
 
     /** Spring容器 */
-    private static ApplicationContext applicationContext;
+    private volatile static ApplicationContext applicationContext;
+
+    /** {@link ApplicationContext}观察者队列 */
+    private static List<Consumer<ApplicationContext>> contextConsumers = new LinkedList<>();
+
+    /** {@link Environment}观察者队列 */
+    private static List<Consumer<Environment>> environmentConsumers = new LinkedList<>();
+
+    private static ReadWriteLock contextLock = new ReentrantReadWriteLock();
+
+    private static ReadWriteLock environmentLock = new ReentrantReadWriteLock();
 
     @Autowired
     private Environment mEnvironment;
 
     public static Environment getEnvironment() {
-        return ENVIRONMENT;
+        if (environment == null) {
+            throw new RuntimeException("Environment还没有初始化");
+        }
+        return environment;
+    }
+
+    public static void getEnvironmentAsync(Consumer<Environment> consumer) {
+        if (environment == null) {
+            Lock lock = environmentLock.writeLock();
+            try {
+                lock.lock();
+                if (environment == null) {
+                    environmentConsumers.add(consumer);
+                } else {
+                    consumer.accept(environment);
+                }
+            } finally {
+                lock.unlock();
+            }
+        } else {
+            consumer.accept(environment);
+        }
     }
 
     public static ApplicationContext getApplicationContext() {
+        if (applicationContext == null) {
+            throw new RuntimeException("ApplicationContext还没有初始化");
+        }
         return applicationContext;
     }
 
+    /**
+     * 用于在Spring执行setApplicationContext前使用ApplicationContext的场景
+     */
+    public static void getApplicationContextAsync(Consumer<ApplicationContext> consumer) {
+        if (applicationContext == null) {
+            Lock lock = contextLock.writeLock();
+            try {
+                lock.lock();
+                if (applicationContext == null) {
+                    contextConsumers.add(consumer);
+                } else {
+                    consumer.accept(applicationContext);
+                }
+            } finally {
+                lock.unlock();
+            }
+        } else {
+            consumer.accept(applicationContext);
+        }
+    }
+
     public static <T> T getBean(Class<T> type) {
-        return (T) applicationContext.getBean(type);
+        return (T) getApplicationContext().getBean(type);
+    }
+
+    /**
+     * 用于在Spring执行setApplicationContext前使用ApplicationContext的场景
+     */
+    public static <T> void getBeanAsync(Class<T> type, Consumer<T> consumer) {
+        getApplicationContextAsync(context -> consumer.accept(context.getBean(type)));
     }
 
     @SuppressWarnings("unchecked")
     public static <T> T getBean(String name) {
-        return (T) applicationContext.getBean(name);
+        return (T) getApplicationContext().getBean(name);
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
         // 注入Environment
-        ENVIRONMENT = mEnvironment;
+        environment = mEnvironment;
+
+        Lock lock = environmentLock.readLock();
+        try {
+            lock.lock();
+            environmentConsumers.forEach(consumer -> consumer.accept(environment));
+            environmentConsumers = null;
+            environmentLock = null;
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) {
         // 注入applicationContext
         SpringContextUtils.applicationContext = applicationContext;
+
+        Lock lock = contextLock.readLock();
+        try {
+            lock.lock();
+            contextConsumers.forEach(consumer -> consumer.accept(applicationContext));
+            contextConsumers = null;
+            contextLock = null;
+        } finally {
+            lock.unlock();
+        }
     }
 }
--- src/main/java/com/ganguomob/dev/myspringboot/Application.java
+++ src/main/java/com/ganguomob/dev/myspringboot/Application.java
@@ -3,9 +3,13 @@ package com.ganguomob.dev.myspringboot;
 import com.ganguomob.dev.myspringboot.socketio.scan.SocketServiceScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @SocketServiceScan
+@EnableAsync
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
